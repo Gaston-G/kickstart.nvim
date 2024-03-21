e9599d6adbc4802b5fd48e9b540121d98e056dcb
Gaston-G <urugaston@gmail.com>
2024-03-20 23:47:57 -0300

    Add keybinds for harpoon.

--- init.lua
+++ init.lua
@@ -769,5 +769,28 @@ require('lazy').setup({
   },
 })
 
+-- NOTE: harpoon keybinds
+
+local harpoon = require 'harpoon'
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set('n', '<leader>a', function()
+  harpoon:list():append()
+end, { desc = 'Adds file to harpoon list.' })
+vim.keymap.set('n', '<C-e>', function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end, { desc = 'Opens harpooned file list menu.' })
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set('n', '<C-S-P>', function()
+  harpoon:list():prev()
+end)
+vim.keymap.set('n', '<C-S-N>', function()
+  harpoon:list():next()
+end)
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
