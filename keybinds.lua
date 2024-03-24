310c053cd36e1353c5878c75ba87e06a7cf9f66c
Gaston-G <urugaston@gmail.com>
2024-03-24 20:07:16 -0300

    Make map function for keybind mapping.

--- keybinds.lua
+++ keybinds.lua
@@ -1,32 +1,36 @@
--- TODO: implement a local func to replace keymap.set
+local map = function(mode, keys, action, description)
+  vim.keymap.set(mode, keys, action, { desc = description })
+end
 
 -- NOTE: Clipboard keybinds
-vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { desc = '[y]ank highlighted text' })
-vim.keymap.set({ 'n', 'v' }, '<leader>Y', '"+Y', { desc = '[Y]ank entire line' })
+map({ 'n', 'v' }, '<leader>y', '"+y', '[y]ank highlighted text')
+map({ 'n', 'v' }, '<leader>Y', '"+Y', '[Y]ank entire line')
 
-vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p', { desc = '[p]aste after cursor' })
-vim.keymap.set({ 'n', 'v' }, '<leader>P', '"+P', { desc = '[P]aste before cursor' })
+map({ 'n', 'v' }, '<leader>p', '"+p', '[p]aste after cursor')
+map({ 'n', 'v' }, '<leader>P', '"+P', '[P]aste before cursor')
 
 -- NOTE: Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+map('n', '[d', vim.diagnostic.goto_prev, 'Go to previous [D]iagnostic message')
+map('n', ']d', vim.diagnostic.goto_next, 'Go to next [D]iagnostic message')
+map('n', '<leader>e', vim.diagnostic.open_float, 'Show diagnostic [E]rror messages')
+map('n', '<leader>q', vim.diagnostic.setloclist, 'Open diagnostic [Q]uickfix list')
 
 -- NOTE: Harpoon keybinds
 local harpoon = require 'harpoon'
 harpoon:setup()
 
-vim.keymap.set('n', '<leader>a', function()
+map('n', '<leader>a', function()
   harpoon:list():append()
-end, { desc = 'Adds file to harpoon list' })
-vim.keymap.set('n', '<C-e>', function()
-  harpoon.ui:toggle_quick_menu(harpoon:list())
-end, { desc = 'Opens harpooned file list menu' })
+end, 'Adds file to harpoon list')
 
-vim.keymap.set('n', '<C-S-P>', function()
+map('n', '<C-e>', function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end, 'Opens harpooned file list menu')
+
+map('n', '<C-S-P>', function()
   harpoon:list():prev()
 end)
-vim.keymap.set('n', '<C-S-N>', function()
+
+map('n', '<C-S-N>', function()
   harpoon:list():next()
 end)
